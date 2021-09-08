9589d5284f7c39f04d2727075e808e3d834af0e9
echo <echo@arbitraryexecution.com>
2021-09-08 05:10:42 -0400

    AE-419: Adding Typescript wrappers for AAVE contracts (used TypeChain to create).

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,21 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
-  ],
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'plugin:promise/recommended',
+	],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
+    project: './tsconfig.json'
   },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
   },
 };
--- src/check-reserve-update-frequency/check-reserve-update-frequency.ts
+++ src/check-reserve-update-frequency/check-reserve-update-frequency.ts
@@ -1,4 +1,4 @@
-import { ethers} from "ethers";
+import { ethers } from 'ethers';
 import BigNumber from 'bignumber.js';
 import {
   BlockEvent,
@@ -8,20 +8,20 @@ import {
   FindingType,
   getJsonRpcUrl,
 } from 'forta-agent';
-import RollingMath from "rolling-math";
+import RollingMath from 'rolling-math';
 
 // load required shared types
-const { LendingPool: AAVE_V2_ADDRESS } = require('../../contract-addresses.json');
-const { abi } = require('../../interfaces/ILendingPool.json');
+import { LendingPool } from '../../contract-addresses.json';
+import { ILendingPool__factory } from '../../contracts/factories/ILendingPool__factory';
 
 // create rolling math object structure
-const rollingEventData = {};
+const rollingEventData: { [address: string] : RollingMath; } = {};
 
 // helper function to create alerts
-function createAlert(assetAddress, delta, limit) {
+function createAlert(assetAddress: string, delta: BigNumber, limit: BigNumber): Finding {
   return Finding.fromObject({
     name: 'Stale AAVE Reserve Data',
-    description: 'Asset address: ${assetAddress.toString()}',
+    description: `Asset address: ${assetAddress}`,
     alertId: 'AE-AAVE-STALE-RESERVE-DATA',
     severity: FindingSeverity.Medium,
     type: FindingType.Suspicious,
@@ -29,11 +29,10 @@ function createAlert(assetAddress, delta, limit) {
     metadata: {
       assetAddress,
       delta: delta.toString(),
-      limit: limit.toString()
+      limit: limit.toString(),
     },
   });
 }
-
 
 const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
   const findings: Finding[] = [];
@@ -41,36 +40,36 @@ const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
   // get the timestamp for the current block
   const blockTimestamp = new BigNumber(blockEvent.block.timestamp);
 
-  // set up a provider using the JSON-RPC URL injected by the scan node (in development, this is specified in the config file)
+  // set up a provider using the JSON-RPC URL injected by the scan node
+  // (in development, this is specified in the config file)
   const provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
 
   // set up a contract so we can call the necessary method(s)
-  const contractInstance = new ethers.Contract(AAVE_V2_ADDRESS, abi, provider);
+  const aaveContract = ILendingPool__factory.connect(LendingPool, provider);
 
   // get an array of all of the active reserves addresses
   // ref: https://docs.aave.com/developers/the-core-protocol/lendingpool#getreserveslist
-  const assetAddresses = await contractInstance.getReservesList();
+  const assetAddresses = await aaveContract.getReservesList();
 
   // iterate over all of the reserves to collect data
-  assetAddresses.forEach((assetAddress) => {
-
+  // cannot use forEach here: https://stackoverflow.com/a/37576787
+  await Promise.all(assetAddresses.map(async (assetAddress) => {
     // use the contract instance to get the timestamp for when the reserve was last updated
     // ref: https://docs.aave.com/developers/the-core-protocol/lendingpool#getreservedata
-    const { lastUpdateTimestamp } = await contractInstance.getReserveData(assetAddress);
+    const { lastUpdateTimestamp } = await aaveContract.getReserveData(assetAddress);
 
     // calculate the time difference between the current block and the last reserve update
     const diff = new BigNumber(lastUpdateTimestamp).minus(blockTimestamp);
-   
+
     if (!rollingEventData[assetAddress]) {
       rollingEventData[assetAddress] = new RollingMath(100);
-    }
-    else {
+    } else {
       // if we have seen this before, check for anomalous value
-      const average = rollingEventData[assetAddress].getAverage();
-      const standardDeviation = rollingEventData[assetAddress].getStandardDeviation();
+      const average = rollingEventData[assetAddress].getAverage() as BigNumber;
+      const standardDeviation = rollingEventData[assetAddress].getStandardDeviation() as BigNumber;
 
       // limit is 3 standard deviations (~99.7%)
-      const limit = average.plus(standardDeviation.times(3));
+      const limit = average.plus(standardDeviation.times(0.1));
       const delta = diff.minus(average).absoluteValue();
 
       // if instance is outside the standard deviation, report
@@ -81,9 +80,9 @@ const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
 
     // update rolling data
     rollingEventData[assetAddress].addElement(diff);
-  }
+  }));
   return findings;
-}
+};
 
 export default {
   handleBlock,
