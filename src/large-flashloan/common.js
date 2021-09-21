a46875f7cae8160a792a8353b6011f40cf0fc6a8
yowl <yowl@arbitraryexecution.com>
2021-09-21 07:24:08 -0700

    grab and decode all transfers and flash loans

--- src/large-flashloan/common.js
+++ src/large-flashloan/common.js
@@ -5,7 +5,7 @@ const { aaveEverestId } = require('../../agent-config.json');
 function createAlert(data) {
   return Finding.fromObject({
     name: `Large flashloan`,
-    description: `Flashloan with ${} eth profit`,
+    description: `Flashloan with TODO eth profit`,
     alertId: `AE-AAVE-LARGE-FLASHLOAN`,
     severity: FindingSeverity.High,
     type: FindingType.Suspicious,
@@ -16,5 +16,4 @@ function createAlert(data) {
 
 module.exports = {
   createAlert,
-  dataFields,
 };
--- src/large-flashloan/large-flashloan.js
+++ src/large-flashloan/large-flashloan.js
@@ -10,124 +10,82 @@ const contractAddresses = require('../../contract-addresses.json');
 
 const { LendingPool: lendingPoolAddr, ProtocolDataProvider: dataProviderAddr } = contractAddresses;
 const { abi: IERC20Abi } = require('../../abi/IERC20.json');
-const { abi: LendingPoolAbi } = require('../../abi/ILendingPool.json');
+const { abi: ILendingPoolAbi } = require('../../abi/ILendingPool.json');
 
+const IERC20 = new ethers.utils.Interface(IERC20Abi);
+const ILendingPool = new ethers.utils.Interface(ILendingPoolAbi);
+
+const transfer = IERC20.getEvent('Transfer');
+const flashLoan = ILendingPool.getEvent('FlashLoan');
+
+const transferTopic = IERC20.getEventTopic(transfer);
+const flashLoanTopic = ILendingPool.getEventTopic(flashLoan);
+
+/*
 // get config settings
 const {
   totalValueAndLiquidity: rawConfig,
 } = require('../../agent-config.json');
+*/
 
 // set up RPC provider
 const provider = new ethers.providers.getDefaultProvider(getJsonRpcUrl());
 
 // set up handle to Aave's LendingPool contract
-const lendingPoolContract = new ethers.Contract(lendingPoolAddr, LendingPoolAbi, provider);
-const dataProviderContract = new ethers.Contract(dataProviderAddr, DataAbi, provider);
+const lendingPoolContract = new ethers.Contract(lendingPoolAddr, ILendingPoolAbi, provider);
 
-// create rolling math object structure
-let rollingLiquidityData = {};
-
-// parses and returns data in a usable format
-async function parseData(dataPromise, reserve) {
-  // settle our rpc call
-  const data = await dataPromise;
-
-  // calculate totals
-  const totalDebt = data.totalStableDebt.add(data.totalVariableDebt);
-  const totalValueLocked = totalDebt.add(data.availableLiquidity);
-
-  // create object with data to return
-  const parsedData = {};
-  dataFields.forEach((field) => {
-    parsedData[field] = data[field];
-  });
-
-  // add our custom fields
-  parsedData.reserve = reserve;
-  parsedData.totalDebt = totalDebt;
-  parsedData.totalValueLocked = totalValueLocked;
-
-  return parsedData;
-}
-
-function provideHandleBlock(RollingMath, config, lendingPool, dataProvider) {
-  rollingLiquidityData = {};
-
-  return async function handleBlock(blockEvent) {
+function provideHandleTransaction() {
+  return async function handleTransaction(txEvent) {
     const findings = [];
+
+    const transfers = [];
+    const flashLoans = [];
+
+    // if aave is not involved it doesn't affect us
+    if (!txEvent.addresses[lendingPoolAddr]) return findings;
+
+    txEvent.logs.forEach((log) => {
+      // if you do not know which contract the event log was generated from, it is not safe to only
+      // match on the topic alone since there may be a different number of indexed arguments
+      try {
+        switch (log.topics[0]) {
+          case transferTopic:
+            transfers.push({ parsed: IERC20.parseLog(log), log });
+            console.log('got transfer topic!');
+            break;
+          case flashLoanTopic:
+            flashLoans.push({ parsed: ILendingPool.parseLog(log), log });
+            console.log('got flashloan topic!');
+            break;
+          default:
+            break;
+        }
+      } catch(e) {}
+    });
+
+    if (flashLoans.length === 0) return findings;
+    console.log(transfers);
+    console.log(flashLoans);
+
+    const balances = {};
+
+    transfers.forEach();
+
+
+    const logs = txEvent.logs.filter();
+
+
+    // rip out
+    const aaveLogs = logs.filter((log) => log.address === lendingPoolAddr);
 
     // override block number so we get data from the block in question
     const override = { blockTag: blockEvent.blockNumber };
 
-    // get reserves and the current liquidity from this block
-    const reserves = await lendingPool.getReservesList({ ...override });
-
-    // create array containing a promise that returns the data for each reserve
-    const reserveData = [];
-    reserves.forEach((reserve) => {
-      // RPC call for per reserve data
-      const dataPromise = dataProvider.getReserveData(reserve, { ...override });
-      reserveData.push(
-        parseData(dataPromise, reserve).catch(() => undefined),
-      );
-    });
-
-    // resolve our requests
-    const resolved = (await Promise.all(reserveData)).filter(
-      (result) => result !== undefined,
-    );
-
-    // process the data
-    resolved.forEach((data) => {
-      const { reserve } = data;
-
-      // initialize the rolling math libraries if they don't exist
-      if (!rollingLiquidityData[reserve]) {
-        rollingLiquidityData[reserve] = {};
-        dataFields.forEach((field) => {
-          rollingLiquidityData[reserve][field] = new RollingMath(config.windowSize);
-        });
-      }
-
-      // loop over data
-      dataFields.forEach((field) => {
-        const observation = new BigNumber(data[field].toHexString());
-        const pastData = rollingLiquidityData[reserve][field];
-
-        // only process data for alerts if we have seen a significant number of blocks
-        if (pastData.getNumElements() > config.minElements) {
-          const average = pastData.getAverage();
-          const standardDeviation = pastData.getStandardDeviation();
-
-          const limit = standardDeviation.times(config.numStds);
-          const delta = observation.minus(average).absoluteValue();
-
-          // alert on differences larger than our limit
-          if (delta.isGreaterThan(limit)) {
-            findings.push(createAlert({
-              field, reserve, observation, average,
-            }));
-          }
-        }
-
-        // add observation to our data
-        pastData.addElement(observation);
-      });
-    });
-
-    return findings;
   };
-}
-
-async function teardownProvider() {
-  await provider.destroy();
 }
 
 // exports
 module.exports = {
-  provideHandleBlock,
-  handleBlock: provideHandleBlock(
-    RollingMathLib, rawConfig, lendingPoolContract, dataProviderContract,
-  ),
-  teardownProvider,
+  provideHandleTransaction,
+  handleTransaction: provideHandleTransaction(),
 };
