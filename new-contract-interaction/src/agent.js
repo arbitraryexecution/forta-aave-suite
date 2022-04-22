035de7a34babbc3112dd55c61f5ee85d07168b82
arbitrary-sparrow <92380866+arbitrary-sparrow@users.noreply.github.com>
2022-04-22 14:23:41 -0400

    Updates to new-contract-interaction bot

--- new-contract-interaction/src/agent.js
+++ new-contract-interaction/src/agent.js
@@ -1,201 +1,213 @@
 const {
-  FindingType,
-  FindingSeverity,
-  Finding,
-  getJsonRpcUrl,
+  Finding, FindingSeverity, FindingType, getEthersProvider,
 } = require('forta-agent');
-const ethers = require('ethers');
-const axios = require('axios');
-
-const DEBUG = false; // enable/disable console logging for debugging
 
 // load configuration data from agent config file
-const {
-  newContractInteraction,
-  aaveEverestId: AAVE_EVEREST_ID,
-} = require('../agent-config.json');
+const config = require('../bot-config.json');
 
-// load required shared types
-let {
-  IncentivesController: incentivesControllerAddress,
-  LendingPool: lendingPoolAddress,
-} = require('../contract-addresses.json');
-const {
-  ProtocolDataProvider: protocolDataProviderAddress,
-} = require('../contract-addresses.json');
-const { abi: protocolDataProviderAbi } = require('../abi/AaveProtocolDataProvider.json');
+// set up a variable to hold initialization data used in the handler
+const initializeData = {};
 
-// convert addresses that need to be matched in transactions to lowercase
-incentivesControllerAddress = incentivesControllerAddress.toLowerCase();
-lendingPoolAddress = lendingPoolAddress.toLowerCase();
-
-// stores aToken addresses
-let aTokenAddresses;
-
-// read the .env file and populate process.env with keys/values
-require('dotenv').config();
-
-// etherscan API components
-// this endpoint will list transactions for a given address, sorted oldest to newest
-const BASE_URL = 'https://api.etherscan.io/api?module=account&action=txlist&address=';
-const OPTIONS = '&startblock=0&endblock=999999999&page=1&offset=10&sort=asc&apikey=';
-const API_KEY = process.env.ETHERSCAN_API_KEY; // free tier is limited to 5 calls/sec
-
-// set up provider for contract interaction
-const provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
-
-// set up the contract object for interacting with the protocolDataProvider contract
-const protocolDataProviderContract = new ethers.Contract(
-  protocolDataProviderAddress, protocolDataProviderAbi, provider,
-);
-
-// print log messages only if DEBUG=true
-function log(message) {
-  if (DEBUG) {
-    console.log(message); // eslint-disable-line no-console
-  }
-}
-
-// computes contract age in days
-// units for currentTime and creationTime are SECONDS
-function getContractAge(currentTime, creationTime) {
-  return Math.floor((currentTime - creationTime) / 60 / (60 * 24));
-}
-
-// gets a list of all aToken addresses currently registered with the ProtocolDataProvider
-// contract
-// the mainnet.json file online (https://aave.github.io/aave-addresses/mainnet.json) appears to
-// be out of sync with the data returned by getAllATokens(), so we take the contract as the source
-// of truth
-async function getATokenAddresses(protocolDataProvider) {
-  const aTokens = await protocolDataProvider.getAllATokens();
-  const tokenAddresses = [];
-  aTokens.forEach((aToken) => {
-    tokenAddresses.push(aToken.tokenAddress.toLowerCase());
-  });
-  return tokenAddresses;
-}
-
-// helper function to create alerts
-function createAlert(address, contractAge) {
-  return Finding.fromObject({
-    name: 'New Contract Interaction',
-    description: `Aave LendingPool transaction with new contract ${address}`,
-    alertId: 'AE-AAVE-NEW-CONTRACT-INTERACTION',
-    severity: FindingSeverity.Medium,
-    type: FindingType.Suspicious,
+// helper function to create alert for contract interaction
+function createContractInteractionAlert(
+  contractName,
+  contractAddress,
+  interactionAddress,
+  findingType,
+  findingSeverity,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const finding = {
+    name: `${protocolName} New Contract Interaction`,
+    description: `The ${contractName} contract interacted with a new contract ${interactionAddress}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-NEW-CONTRACT-INTERACTION`,
+    type: FindingType[findingType],
+    severity: FindingSeverity[findingSeverity],
+    protocol: protocolName,
     metadata: {
-      address,
-      contractAge,
+      contractName,
+      contractAddress,
+      interactionAddress,
     },
-    everestId: AAVE_EVEREST_ID,
-  });
+  };
+
+  return Finding.fromObject(finding);
 }
 
-function provideHandleTransaction(ethersProvider, protocolDataProvider) {
+// helper function to create alert for EOA interaction
+function createEOAInteractionAlert(
+  contractName,
+  contractAddress,
+  interactionAddress,
+  transactionCount,
+  findingType,
+  findingSeverity,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const finding = {
+    name: `${protocolName} New EOA Interaction`,
+    description: `The ${contractName} contract interacted with a new EOA ${interactionAddress}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-NEW-EOA-INTERACTION`,
+    type: FindingType[findingType],
+    severity: FindingSeverity[findingSeverity],
+    protocol: protocolName,
+    metadata: {
+      contractName,
+      contractAddress,
+      interactionAddress,
+      transactionCount,
+    },
+  };
+
+  return Finding.fromObject(finding);
+}
+
+function provideInitialize(data) {
+  return async function initialize() {
+    /* eslint-disable no-param-reassign */
+    data.provider = getEthersProvider();
+    data.contractInfo = config.contracts;
+    data.developerAbbreviation = config.developerAbbreviation;
+    data.protocolName = config.protocolName;
+    data.protocolAbbreviation = config.protocolAbbreviation;
+
+    data.contracts = Object.entries(data.contractInfo).map(([name, entry]) => {
+      const {
+        thresholdBlockCount,
+        thresholdTransactionCount,
+        address,
+        filteredAddresses,
+        findingType,
+        findingSeverity,
+      } = entry.newContractEOA;
+
+      const contract = {
+        name,
+        address,
+        filteredAddresses,
+        thresholdBlockCount,
+        thresholdTransactionCount,
+        findingType,
+        findingSeverity,
+      };
+
+      return contract;
+    });
+  /* eslint-enable no-param-reassign */
+  };
+}
+
+function provideHandleTransaction(data) {
   return async function handleTransaction(txEvent) {
+    const {
+      contracts, developerAbbreviation, protocolName, protocolAbbreviation,
+    } = data;
+
     const findings = [];
 
-    // for performance reasons, don't continue to run this handler if an Etherscan API key
-    // was not provided
-    if (API_KEY === undefined) {
-      return findings;
-    }
-
-    // get atoken addresses (one-time operation)
-    // these addresses will be seen interacting with the lending pool in transactions,
-    // so we filter them out later
-    if (aTokenAddresses === undefined) {
-      aTokenAddresses = await getATokenAddresses(protocolDataProvider);
-    }
-
     // get all addresses involved with this transaction
-    let addresses = Object.keys(txEvent.addresses);
+    const transactionAddresses = Object.keys(txEvent.addresses);
 
-    // to minimize Etherscan requests (slow and rate limited to 5/sec for the free tier),
-    // exclude the lending pool address, the incentives controller, and all token addresses
-    let exclusions = [
-      lendingPoolAddress,
-      incentivesControllerAddress,
-    ];
-    exclusions = exclusions.concat(aTokenAddresses);
+    // iterate over all of the contracts from the configuration file
+    await Promise.all(contracts.map(async (contract) => {
+      const {
+        name,
+        address,
+        filteredAddresses,
+        thresholdBlockCount,
+        thresholdTransactionCount,
+        findingType,
+        findingSeverity,
+      } = contract;
 
-    // filter transaction addresses to remove Aave contract addresses
-    addresses = addresses.filter((item) => exclusions.indexOf(item) < 0);
+      let exclusions = [
+        address,
+      ];
+      exclusions = exclusions.concat(filteredAddresses);
+      // filter transaction addresses to remove specified addresses
+      const filteredTransactionAddresses = transactionAddresses
+        .filter((item) => !exclusions.includes(item));
 
-    // watch for recently created contracts interacting with Aave lending pool
-    if (txEvent.transaction.to === lendingPoolAddress) {
-      // create an array of promises that retrieve the contract code for each address
-      const contractCodePromises = [];
-      let contractCode = [];
-      addresses.forEach((address) => {
-        // RPC call for contract code
-        const codePromise = ethersProvider.getCode(address);
+      // watch for recently created contracts interacting with configured contract address
+      if (txEvent.transaction.to === address) {
+        const contractResults = {};
+        const eoaAddresses = [];
 
-        // associate each address with the code that was returned
-        codePromise
-          .then((result) => contractCode.push({ address, code: result }))
-          // to prevent Promise.all() from rejecting, catch failed promises and set the return
-          // value to undefined
-          .catch(() => contractCodePromises.push(undefined));
-      });
+        const results = await Promise.allSettled(
+          filteredTransactionAddresses.map(async (transactionAddress) => {
+            const contractCode = await data.provider.getCode(transactionAddress);
+            return { transactionAddress, code: contractCode };
+          }),
+        );
 
-      // wait for the promises to be settled
-      await Promise.all(contractCodePromises);
+        results.forEach((result) => {
+          if (result.status === 'fulfilled') {
+            if (result.value.code !== '0x') { // if theres code, then its a contract
+              contractResults[result.value.transactionAddress] = result.value.code;
+            } else {
+              eoaAddresses.push(result.value.transactionAddress); // if no code, then its an EOA
+            }
+          }
+        });
 
-      // filter out EOAs from our original list of addresses
-      contractCode = contractCode.filter((item) => (item.code !== '0x'));
-      addresses = contractCode.map((item) => item.address);
+        await Promise.all(eoaAddresses.map(async (eoaAddress) => {
+          const eoaTransactionCount = await data.provider.getTransactionCount(eoaAddress);
 
-      // Next, for each contract, query the Etherscan API for the list of transactions
-      // the first transaction item returned in the results will be the earliest
-      const etherscanTxlistPromises = [];
-      const txData = [];
-      contractCode.forEach((item) => {
-        const txlistPromise = axios.get(BASE_URL + item.address + OPTIONS + API_KEY);
+          if (eoaTransactionCount < thresholdTransactionCount) {
+            findings.push(createEOAInteractionAlert(
+              name,
+              address,
+              eoaAddress,
+              eoaTransactionCount,
+              findingType,
+              findingSeverity,
+              protocolName,
+              protocolAbbreviation,
+              developerAbbreviation,
+            ));
+          }
+        }));
 
-        // associate each address with the transaction list that was returned
-        txlistPromise
-          .then((result) => txData.push({ address: item.address, response: result }))
-          // to prevent Promise.all() from rejecting, catch failed promises and set the return
-          // value to undefined
-          .catch(() => etherscanTxlistPromises.push(undefined));
-      });
+        const blockOverride = txEvent.blockNumber - thresholdBlockCount;
+        const blockResults = await Promise.allSettled(
+          Object.keys(contractResults).map(async (contractResult) => {
+            const contractCode = await data.provider.getCode(contractResult, blockOverride);
+            return { transactionAddress: contractResult, code: contractCode };
+          }),
+        );
 
-      // wait for the promises to be settled
-      await Promise.all(etherscanTxlistPromises);
+        blockResults.forEach((result) => {
+          if (result.status === 'fulfilled') {
+            if (result.value.code !== contractResults[result.value.transactionAddress]) {
+              findings.push(createContractInteractionAlert(
+                name,
+                address,
+                result.value.transactionAddress,
+                findingType,
+                findingSeverity,
+                protocolName,
+                protocolAbbreviation,
+                developerAbbreviation,
+              ));
+            }
+          }
+        });
+      }
+    }));
 
-      // process the results
-      txData.forEach((item) => {
-        // bail if the API did not return a valid result
-        if (item.response.data.status === 0) {
-          return;
-        }
-
-        // get the timestamp from the earliest transaction
-        const creationTime = item.response.data.result[0].timeStamp;
-
-        // compute days elapsed since contract creation
-        const currentTime = txEvent.timestamp;
-        log(`Contract address: ${item.address}`);
-        const contractAge = getContractAge(currentTime, creationTime);
-        log(`Contract was created ${contractAge} days ago`);
-
-        // filter on recent contracts (default value is 7 days; defined in agent-config.json)
-        if (contractAge < newContractInteraction.thresholdAgeDays) {
-          log(`Contract ${item.address} was recently created`);
-          findings.push(createAlert(item.address, contractAge));
-        }
-      });
-    }
     return findings;
   };
 }
 
 module.exports = {
+  provideInitialize,
+  initialize: provideInitialize(initializeData),
   provideHandleTransaction,
-  handleTransaction: provideHandleTransaction(provider, protocolDataProviderContract),
-  getContractAge,
-  createAlert,
-  lendingPoolAddress,
+  handleTransaction: provideHandleTransaction(initializeData),
+  createContractInteractionAlert,
+  createEOAInteractionAlert,
 };
