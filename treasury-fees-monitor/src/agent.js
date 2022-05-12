a90b8007a975595e8835ed0bf962a816ef8fa8f8
arbitrary-sparrow <92380866+arbitrary-sparrow@users.noreply.github.com>
2022-05-12 10:40:03 -0400

    First pass at working bot

--- treasury-fees-monitor/src/agent.js
+++ treasury-fees-monitor/src/agent.js
@@ -15,15 +15,24 @@ const initializeData = {};
 
 const DECIMALS_ABI = ['function decimals() view returns (uint)'];
 
-function createAlert(protocolName, protocolAbbrev, developerAbbrev, type, severity) {
+function createAlert(protocolName, protocolAbbrev, developerAbbrev, type, severity, tokenInfo) {
+  const {
+    tokenAsset,
+    tokenPriceEth,
+    premiumEth,
+  } = tokenInfo;
+
   return Finding.fromObject({
     name: `${protocolName} Treasury Fee Monitor`,
-    description: '',
+    description: `An anomalous flash loan premium of ${premiumEth} ETH was paid to the treasury`,
     alertId: `${developerAbbrev}-${protocolAbbrev}-TREASURY-FEE`,
     type: FindingType[type],
     severity: FindingSeverity[severity],
     protocol: protocolName,
     metadata: {
+      tokenAsset,
+      tokenPriceEth: tokenPriceEth.toString(),
+      premiumEth: premiumEth.toString(),
     },
   });
 }
@@ -69,11 +78,11 @@ async function parseCsvAndCompute(csvFileName, tokenInfo, tokenPriceInfo) {
         const scaledPremium = parseFloat(
           (new BigNumber(data.premium)).div(denominator),
         );
-        const premiumInETH = scaledPremium * tokenPriceInfo[data.asset];
+        const premiumInEth = scaledPremium * tokenPriceInfo[data.asset];
 
         ({
           mean, stdDev, variance, numDataPoints,
-        } = calculateStatistics(mean, variance, numDataPoints, premiumInETH));
+        } = calculateStatistics(mean, variance, numDataPoints, premiumInEth));
       })
       .on('end', () => resolve({
         mean, stdDev, variance, numDataPoints,
@@ -89,7 +98,8 @@ function provideInitialize(data) {
     data.protocolName = config.protocolName;
     data.protocolAbbreviation = config.protocolAbbreviation;
     data.developerAbbreviation = config.developerAbbreviation;
-    data.provider = getEthersProvider();
+    data.type = config.type;
+    data.severity = config.severity;
 
     const {
       LendingPoolAddressesProvider: lendingPoolAddressesProvider,
@@ -97,13 +107,14 @@ function provideInitialize(data) {
       ProtocolDataProvider: protocolDataProvider,
       PriceOracle: priceOracle,
     } = config.contracts;
+    const provider = getEthersProvider();
 
     // from the LendingPoolAddressesProvider, get the address of the LendingPool proxy contract
     const lendingPoolAddressesProviderAbi = getAbi(lendingPoolAddressesProvider.abiFile);
     data.lendingPoolAddressesProviderContract = new ethers.Contract(
       lendingPoolAddressesProvider.address,
       lendingPoolAddressesProviderAbi,
-      data.provider,
+      provider,
     );
     data.lendingPoolAddress = await data.lendingPoolAddressesProviderContract.getLendingPool();
 
@@ -111,18 +122,16 @@ function provideInitialize(data) {
     const lendingPoolAbi = getAbi(lendingPool.abiFile);
     const lendingPoolInterface = new ethers.utils.Interface(lendingPoolAbi);
 
-    // retrieve the event signature (aka topic hash) for the FlashLoan event from the interface
-    const sigTypeHash = ethers.utils.FormatTypes.sighash;
-    data.flashLoanTopicHash = ethers.utils.id(
-      lendingPoolInterface.getEvent('FlashLoan').format(sigTypeHash),
-    );
+    // retrieve the event signature for the FlashLoan event from the interface
+    const sigTypeFull = ethers.utils.FormatTypes.full;
+    data.flashLoanSignature = lendingPoolInterface.getEvent('FlashLoan').format(sigTypeFull);
 
     const { address: protocolDataProviderAddress } = protocolDataProvider;
     const protocolDataProviderAbi = getAbi(protocolDataProvider.abiFile);
     const protocolDataProviderContract = new ethers.Contract(
       protocolDataProviderAddress,
       protocolDataProviderAbi,
-      data.provider,
+      provider,
     );
 
     // get all aToken addresses from the protocol data provider contract
@@ -133,7 +142,7 @@ function provideInitialize(data) {
       // token is an array where the first element is the token symbol and the second element is
       // the address
       const tokenAddress = token[1];
-      const tokenContract = new ethers.Contract(tokenAddress, DECIMALS_ABI, data.provider);
+      const tokenContract = new ethers.Contract(tokenAddress, DECIMALS_ABI, provider);
       const tokenDecimals = await tokenContract.decimals();
       data.tokenInfo[tokenAddress] = tokenDecimals.toString();
     }));
@@ -144,20 +153,20 @@ function provideInitialize(data) {
     data.priceOracleContract = new ethers.Contract(
       priceOracleAddress,
       priceOracleAbi,
-      data.provider,
+      provider,
     );
 
     // for each asset retrieve a spot price that will be used when computing statistics
     const tokenAddressesList = tokenAddresses.map((token) => token[1]);
     // the spot prices given by the Aave price oracle are in 'ETH wei' units
-    const denominatorWEI = (new BigNumber(10)).pow(18);
-    const spotPricesScaledETH = {};
+    const denominatorWei = (new BigNumber(10)).pow(18);
+    const spotPricesScaledEth = {};
     // request all of the prices for the assets in tokenAddressesList from the price oracle
-    const spotPricesETH = await data.priceOracleContract.getAssetsPrices(tokenAddressesList);
+    const spotPricesEth = await data.priceOracleContract.getAssetsPrices(tokenAddressesList);
     // iterate over each price, correlate it with its associated asset token, and scale the price
-    spotPricesETH.forEach((price, index) => {
-      spotPricesScaledETH[tokenAddressesList[index]] = parseFloat(
-        (new BigNumber(price.toString())).div(denominatorWEI),
+    spotPricesEth.forEach((price, index) => {
+      spotPricesScaledEth[tokenAddressesList[index]] = parseFloat(
+        (new BigNumber(price.toString())).div(denominatorWei),
       );
     });
 
@@ -165,11 +174,11 @@ function provideInitialize(data) {
     // and variance
     const {
       mean, stdDev, variance, numDataPoints,
-    } = await parseCsvAndCompute(config.dataSet, data.tokenInfo, spotPricesScaledETH);
-    console.log(mean);
-    console.log(stdDev);
-    console.log(variance);
-    console.log(numDataPoints);
+    } = await parseCsvAndCompute(config.dataSet, data.tokenInfo, spotPricesScaledEth);
+    data.mean = mean;
+    data.stdDev = stdDev;
+    data.variance = variance;
+    data.numDataPoints = numDataPoints;
     /* eslint-enable no-param-reassign */
   };
 }
@@ -179,6 +188,74 @@ function provideHandleTransaction(data) {
     // check for FlashLoan event, if found then check to see if value is a statistical anomaly given
     // the current mean + standard deviation (i.e. 3 std deviations or more larger than mean), if so
     // return a finding, finally re-calculate mean and standard deviation
+    const {
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+      lendingPoolAddress,
+      flashLoanSignature,
+      tokenInfo,
+      priceOracleContract,
+      type,
+      severity,
+    } = data;
+
+    let {
+      mean,
+      stdDev,
+      variance,
+      numDataPoints,
+    } = data;
+
+    const parsedLogs = txEvent.filterLog(flashLoanSignature, lendingPoolAddress);
+    const findings = (await Promise.all(parsedLogs.map(async (log) => {
+      const finding = [];
+      // scale the found premium using the decimals value in tokenInfo
+      const denominator = (new BigNumber(10)).pow(tokenInfo[log.args.asset]);
+      const scaledPremium = parseFloat(
+        (new BigNumber(log.args.premium.toString())).div(denominator),
+      );
+
+      // query the price oracle for the current price of the token asset in ETH
+      const ethDenominator = (new BigNumber(10)).pow(18);
+      let tokenPriceEth = await priceOracleContract.getAssetPrice(log.args.asset);
+      tokenPriceEth = parseFloat((new BigNumber(tokenPriceEth.toString())).div(ethDenominator));
+
+      const scaledPremiumInEth = scaledPremium * tokenPriceEth;
+      // if the scaled premium (in eth) of the flash loan is greater than the
+      // mean + 3 standard deviations generate a finding
+      if (scaledPremiumInEth > (mean + (3 * stdDev))) {
+        const loanInfo = {
+          tokenAsset: log.args.asset,
+          tokenPriceEth,
+          premiumEth: scaledPremiumInEth,
+        };
+
+        finding.push(createAlert(
+          protocolName,
+          protocolAbbreviation,
+          developerAbbreviation,
+          type,
+          severity,
+          loanInfo,
+        ));
+      }
+
+      // update the statistics with the new value
+      ({
+        mean, stdDev, variance, numDataPoints,
+      } = calculateStatistics(mean, variance, numDataPoints, scaledPremiumInEth));
+      /* eslint-disable no-param-reassign */
+      data.mean = mean;
+      data.stdDev = stdDev;
+      data.variance = variance;
+      data.numDataPoints = numDataPoints;
+      /* eslint-enable no-param-reassign */
+
+      return finding;
+    }))).flat();
+
+    return findings;
   };
 }
 
